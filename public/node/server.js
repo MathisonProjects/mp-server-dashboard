91adeb0d296f98d26febf8fdac9f9b4648c4bd61
Jacob Mathison <jacob@mathisonprojects.com>
2019-09-06 13:56:30 -0500

    Created nodeserver for handling cloudflare. Can programatically change development mode to cached mode. NodeJs server now available.

--- public/node/server.js
+++ public/node/server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env nodejs
+require('dotenv').config();
 var express = require('express'),
     http = require('http');
 var app = express();
@@ -8,18 +9,18 @@ var io = require('socket.io').listen(server);
 
 var cf = require('cloudflare')({
   email: 'admin+cloudflare@zing.land',
-  key  : 'your Cloudflare API key' // .ENV cloudflare token should be stored.
+  key  : process.env.CLOUDFLARE_READ_APIKEY
 });
 
 app.get('/', function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World! Node.js is working correctly on ' + port + ".");
+	res.end('Mathison Projects Dashboard Node Server on ' + port + ".");
 });
 
 io.on('connection', function(socket){
-	console.log('A connection has been established.');
+	runConsole('A connection has been established.');
 
-	socket.emit('init payload', initPayload);
+	socket.emit('init response');
 
 	socket.on('connect', function(){
 		console.log('Connect to server');
@@ -29,8 +30,40 @@ io.on('connection', function(socket){
 	socket.on('fireCloudflare', function(endpoint, type) {
 
 	});
+
+	socket.on('getDevelopmentMode', function() {
+		runConsole('Getting Mode...');
+		cf.zones.read('40d98dc76a92a76456f86f55850a51c5/settings/development_mode')
+		.then( response => {
+
+			socket.emit('ModeResponse', response);
+		})
+		.catch( error => {
+			console.log(error);
+		});
+	});
+
+	socket.on('changeDevelopmentMode', function(mode) { // string "on"/"off"
+		runConsole('Changing Mode...');
+		cf.zones.edit('40d98dc76a92a76456f86f55850a51c5/settings/development_mode', { value : mode })
+			.then( response => {
+				socket.emit('ModeResponse', response);
+			})
+			.catch( error => {
+				console.log(error);
+			});
+	});
 });
 
 server.listen(port, function(){
 	console.log('listening on *:'+port);
 });
+
+// node -r dotenv/config server.js dotenv_config_path=/var/www/html/live/dashboard.mathisonprojects.com/.env
+
+
+function runConsole(info) {
+	console.log('-----------------------');
+	console.log(info);
+	console.log('-----------------------');
+}
