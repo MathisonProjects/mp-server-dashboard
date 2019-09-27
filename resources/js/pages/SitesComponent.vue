d3040f4b0db99a66bf416462af34a3fa04f3a3b0
Jacob Mathison <jacob@mathisonprojects.com>
2019-09-27 09:16:37 -0500

    Worked on presentation for the sites management page. Looks nice. Need to wrap up final details on site deployment, store it in a DB, and have it pullable from this page.

--- resources/js/pages/SitesComponent.vue
+++ resources/js/pages/SitesComponent.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div class='row'>
+		<div class='row my-3'>
 			<div class='col'>
 				<h1>Sites</h1>
 			</div>
@@ -19,14 +19,53 @@
 				</ul>
 			</div>
 			<div class='col'>
-				Site Name
-				Delete | Edit | Redeploy | To Test | To Live
+				<div class="card">
+				  <div class="card-header">
+				    Site Name
+				  </div>
+				  <div class="card-body">
 
-				Name         -
-				Description  - 
-				URLs         -
-				Virtual Host -
-				Node Status  - 
+				  	<div class='row'>
+				  		<div class='col'>
+				  			<button type='button' class='btn btn-danger btn-block'><i class='fas fa-trash'></i> Delete</button>
+				  		</div>
+				  		<div class='col'>
+				  			<button type='button' class='btn btn-warning btn-block'><i class='fas fa-edit'></i> Edit</button>
+				  		</div>
+				  		<div class='col'>
+				  			<button type='button' class='btn btn-primary btn-block'><i class='fas fa-trash-restore'></i> Redeploy</button>
+				  		</div>
+				  		<div class='col'>
+				  			<button type='button' class='btn btn-info btn-block'><i class='fab fa-dev'></i> To Test</button>
+				  		</div>
+				  		<div class='col'>
+				  			<button type='button' class='btn btn-success btn-block'><i class='fas fa-power-off'></i> To Live</button>
+				  		</div>
+				  	</div>
+
+
+				    <h5 class="card-title">{{ currentSiteData.name }}</h5>
+				    <p class="card-text" v-if='currentSiteData.node'><b>Node Status:</b> <span class='text-success'><i class="fas fa-circle"></i> Active</span></p>
+				    <p class="card-text" v-if='!currentSiteData.node'><b>Node Status:</b> <span class='text-danger'><i class="fas fa-circle"></i> Inactive</span></p>
+				    <p class="card-text">{{ currentSiteData.description }}</p>
+				    
+
+				    <ul>
+				    	<li>Original Git: <a href='javascript:void(0)'>{{ currentSiteData.git.original }}</a></li>
+				    	<li>Current Git: <a href='javascript:void(0)'>{{ currentSiteData.git.new }}</a></li>
+				    </ul>
+
+				    <ul>
+				    	<li>Dev: <a href='javascript:void(0)'>https://{{ currentSiteData.links.dev }}</a></li>
+				    	<li>Test: <a href='javascript:void(0)'>https://{{ currentSiteData.links.test }}</a></li>
+				    	<li>Live  <a href='javascript:void(0)'>https://{{ currentSiteData.links.live }}</a></li>
+				    </ul>
+
+					<p class="card-text">Virtual Host</p>
+					<pre>{{ currentSiteData.virtualHost }}</pre>
+				  </div>
+				</div>
+
 			</div>
 		</div>
 
@@ -56,6 +95,23 @@
 		computed  : {
 			siteList() {
 				return this.$store.state.sitesStore.sites;
+			},
+			currentSiteData() {
+				return {
+					name: 'Site Name',
+					description: 'This is a description of a site...',
+					git: {
+						original: 'https://github.com/laravel/laravel.git',
+						new     : 'https://github.com/Divinityfound/IgnoreThisRepo-APITest'
+					},
+					node: true,
+					links: {
+						dev : 'dev-thisisatest-com.mathisonprojects.dev',
+						test: 'dev-thisisatest-com.mathisonprojects.dev',
+						live: 'thisisatest.com'
+					},
+					virtualHost: 'VIRTUAL HOST INFO PENDING'
+				}
 			}
 		},
 		methods   : {
@@ -78,4 +134,9 @@
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	pre {
+		background: #585858;
+		color: white;
+	}
+</style>
